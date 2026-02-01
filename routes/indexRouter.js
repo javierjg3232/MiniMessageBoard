3e7d599ef2377dbde2e2280495522b2e8d994b8e
javierjg3232 <143865913+javierjg3232@users.noreply.github.com>
2026-02-01 05:15:17 -0800

    Create a view and route to add new messages
    Setup view with form for new messages. One input
    for message text and one for user name. Submit
    button to post message with POST request to /new.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -19,4 +19,8 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
 
+indexRouter.get("/new", (req, res) => {
+  res.render("newMessageForm");
+});
+
 module.exports = indexRouter;
